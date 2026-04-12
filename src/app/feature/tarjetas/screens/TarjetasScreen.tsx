6a79cac0eb79c42c33d0af62cba9542d9fa2210c
RoemDev48 <rodrigoerodriguez48@gmail.com>
2026-04-11 22:46:54 -0300

    FIX: Estetico

--- src/app/feature/tarjetas/screens/TarjetasScreen.tsx
+++ src/app/feature/tarjetas/screens/TarjetasScreen.tsx
@@ -1,11 +1,12 @@
 import { usePersonalization } from "@/src/app/contexts/PersonalizationContext";
+import BackButton from "@/src/app/feature/common/BackButton";
 import CustomText from "@/src/app/feature/common/CustomText";
+import ScreenTitle from "@/src/app/feature/common/ScreenTitle";
 import RootStackParamsList from "@/src/app/navigation/navigation.types";
-import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import React, { useMemo } from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ZenithXAnimado from "../../../assets/icon/ZenithXAnimado.svg";
 
 const CardsScreen = () => {
@@ -19,18 +20,6 @@ const CardsScreen = () => {
         container: {
           flex: 1,
           backgroundColor: themedColors.background,
-        },
-        backButton: {
-          flexDirection: "row",
-          alignItems: "center",
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-          gap: 4,
-        },
-        backText: {
-          fontSize: 18,
-          fontWeight: "500",
-          color: themedColors.text,
         },
         content: {
           flex: 1,
@@ -54,15 +43,9 @@ const CardsScreen = () => {
 
   return (
     <View style={styles.container}>
-      {/* Botón Atrás */}
-      <TouchableOpacity
-        onPress={navigation.goBack}
-        style={styles.backButton}
-        activeOpacity={0.7}
-      >
-        <Ionicons name="chevron-back" size={28} color={themedColors.text} />
-        <CustomText style={styles.backText}>Atrás</CustomText>
-      </TouchableOpacity>
+      <BackButton onPress={() => navigation.goBack()} />
+
+      <ScreenTitle text="Tarjetas" />
 
       {/* Contenido centrado */}
       <View style={styles.content}>
